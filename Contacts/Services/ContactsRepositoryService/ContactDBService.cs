d819d03e63844aab6631a77397abfcfa7c0caf97
Bekzod Allaev <Allaev_@outlook.com>
2019-01-08 15:04:28 +0500

    Added Contacts DataBaseService

---
+++ Contacts/Services/ContactsRepositoryService/ContactDBService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contacts.Models;
+
+namespace Contacts.Services.ContactsRepositoryService
+{
+    class ContactDBService : IContactRepositoryService
+    {
+        public Task DeleteAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Contact>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
