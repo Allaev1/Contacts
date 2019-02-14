3ac5b1cc4b1edd7e3d2ab6c6c86df00d191971df
Bekzod Allaev <Allaev_@outlook.com>
2019-02-14 16:35:12 +0500

    Added proxy object and made some changes to AddEditPageViewModel

---
+++ Contacts/ProxyModels/ProxyContact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts.ProxyModels
+{
+    public class ProxyContact
+    {
+        public String FirstName { get; set; }
+
+        public String LastName { get; set; }
+
+        public String PhoneNumber { get; set; }
+
+        public String PathToImage { get; set; }
+
+        public String Email { get; set; }
+
+        public Int64? IsFavorite { get; set; }
+
+        public Int64? GroupID { get; set; }
+    }
+}
--- Contacts/ViewModels/AddEditPageViewModel.cs
+++ Contacts/ViewModels/AddEditPageViewModel.cs
@@ -4,13 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Template10.Mvvm;
+using Windows.UI.Xaml.Navigation;
 
 namespace Contacts.ViewModels
 {
     class AddEditPageViewModel : ViewModelBase
     {
-        #region Navigation events
+        #region Fields
 
+        #endregion
+
+        #region Constructors
+        public AddEditPageViewModel()
+        {
+
+        }
+        #endregion
+
+        #region Navigation events
+        public override Task OnNavigatedToAsync(object parameter, NavigationMode mode, IDictionary<string, object> state)
+        {
+            return base.OnNavigatedToAsync(parameter, mode, state);
+        }
         #endregion
     }
 }
