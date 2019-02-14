24237f59f2232243c68c5d71f5c3c642fb6ee5de
Bekzod Allaev <Allaev_@outlook.com>
2019-02-15 00:28:31 +0500

    ProxyContact was inherited by ValidatableModelBase

--- Contacts/ProxyModels/ProxyContact.cs
+++ Contacts/ProxyModels/ProxyContact.cs
@@ -3,23 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Template10.Validation;
+using Contacts.Models;
 
 namespace Contacts.ProxyModels
 {
-    public class ProxyContact
+    public class ProxyContact : ValidatableModelBase
     {
-        public String FirstName { get; set; }
+        #region Constructors
+        public ProxyContact(Models.Contacts contact)
+        {
+            FirstName = contact.FirstName;
+            LastName = contact.LastName;
+            PhoneNumber = contact.PhoneNumber;
+            //PathToImage = contact.PathToImage;
+            Email = contact.Email;
+            IsFavorite = contact.IsFavorite;
+            GroupID = contact.GroupID;
+        }
+        #endregion
 
-        public String LastName { get; set; }
+        public String FirstName { get { return Read<string>(); } set { Write(value); } }
 
-        public String PhoneNumber { get; set; }
+        public String LastName { get { return Read<string>(); } set { Write(value); } }
 
-        public String PathToImage { get; set; }
+        public String PhoneNumber { get { return Read<string>(); } set { Write(value); } }
 
-        public String Email { get; set; }
+        //public String PathToImage { get { return Read<string>(); } set { Write(value); } }
 
-        public Int64? IsFavorite { get; set; }
+        public String Email { get { return Read<string>(); } set { Write(value); } }
 
-        public Int64? GroupID { get; set; }
+        public Int64? IsFavorite { get { return Read<Int64?>(); } set { Write(value); } }
+
+        public Int64? GroupID { get { return Read<Int64?>(); } set { Write(value); } }
+
+        #region Calculated properties
+        public string FullName
+        {
+            get { return $"{FirstName} {LastName}"; }
+        }
+        #endregion
     }
 }
--- Contacts/ViewModels/AddEditPageViewModel.cs
+++ Contacts/ViewModels/AddEditPageViewModel.cs
@@ -5,26 +5,29 @@ using System.Text;
 using System.Threading.Tasks;
 using Template10.Mvvm;
 using Windows.UI.Xaml.Navigation;
+using Contacts.Services.ContactsRepositoryService;
 
 namespace Contacts.ViewModels
 {
     class AddEditPageViewModel : ViewModelBase
     {
         #region Fields
-
+        IContactRepositoryService repositoryService;
         #endregion
 
         #region Constructors
         public AddEditPageViewModel()
         {
-
+            repositoryService = new ContactDBService();
         }
         #endregion
 
         #region Navigation events
         public override Task OnNavigatedToAsync(object parameter, NavigationMode mode, IDictionary<string, object> state)
         {
-            return base.OnNavigatedToAsync(parameter, mode, state);
+
+
+            return Task.CompletedTask;
         }
         #endregion
     }
